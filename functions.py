1a8b4a0672e0c684b04c10e1a5185e0208610db2
Ritika Mishra <mishra.r.0804@gmail.com>
2023-11-22 12:36:27 +0530

    Function done

---
+++ functions.py
@@ -0,0 +1,15 @@
+# function
+
+# def myFunction():
+#     #print("This is a function")
+#     a=10
+#     b=30
+#     return a+b
+# b=myFunction()
+# print(b)
+
+
+def minus(a,b):
+    return a-b
+
+print(minus(100,20))
