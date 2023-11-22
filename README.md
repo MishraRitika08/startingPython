df6e6323627680122d3cf17132d71f2b05863f33
Ritika Mishra <mishra.r.0804@gmail.com>
2023-11-22 11:41:36 +0530

    first

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # startingPython
+# A workshop is being organised in our college on 22 Nov,2023.
--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print("Hello World")
+
+a = 2
+b = 1.0
+c = "Hello"
+
+print(type(a))
+print(type(b))
+print(type(c))
