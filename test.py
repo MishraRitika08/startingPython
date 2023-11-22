c11221dc2e93e8315a6848e1f233fd94d66e606f
Ritika Mishra <mishra.r.0804@gmail.com>
2023-11-22 12:11:27 +0530

    if else statement

--- test.py
+++ test.py
@@ -1,13 +1,27 @@
 #print("Hello World")
 
 #Data Types
-a = 10
-b = 1.0
-'''' c = a+b
+# a = input()
+# b = 10
+'''' c = a+b2
  d = "happy"'''
 # e =  "birthday"
 # #print(type(a+b))
 # print((d+e))
-# print(type(d+e))
+#print(type (a))
 
+
+
+# If - else Statement
+
+if(a==10):
+    print("True")
+    print(a)
+elif(a==34):
+    print("hello")
+    
+else:
+    print("False")
+
+print('statement')
 
