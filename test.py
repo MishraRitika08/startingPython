864ab704cdf6d5a7d8ea8cfa77b03da1d8223d70
Ritika Mishra <mishra.r.0804@gmail.com>
2023-11-22 11:57:04 +0530

    Data types

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 #print("Hello World")
 
-a = 2
+#Data Types
+a = 10
 b = 1.0
-c = a+b
-d = "happy"
-e =  "birthday"
-#print(type(a+b))
-print((d+e))
-print(type(d+e))
+'''' c = a+b
+ d = "happy"'''
+# e =  "birthday"
+# #print(type(a+b))
+# print((d+e))
+# print(type(d+e))
+
+
